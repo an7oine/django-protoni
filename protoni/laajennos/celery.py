4d24ad0a30c614199b35949bade7de08571b43f6
Antti Hautaniemi <antti.hautaniemi@me.com>
2021-05-15 14:07:13 +0300

    Celery-laajennos: lisätään myös asennettuihin sovelluksiin

--- protoni/laajennos/celery.py
+++ protoni/laajennos/celery.py
@@ -3,4 +3,11 @@
 import celery
 del celery
 
-CELERY_BROKER_URL = CONFIG('CELERY_BROKER_URL', default='redis://localhost:6379')
+CELERY_BROKER_URL = CONFIG(
+  'CELERY_BROKER_URL', default='redis://localhost:6379/0'
+)
+CELERY_RESULT_BACKEND = CONFIG(
+  'CELERY_BROKER_URL', default='redis://localhost:6379/0'
+)
+
+INSTALLED_APPS += ['celery']
