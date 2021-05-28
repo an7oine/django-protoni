1b381fa556a1d01128286e4a1e716f86a3ebd5bb
Antti Hautaniemi <antti.hautaniemi@me.com>
2021-05-28 19:20:29 +0300

    Korjattu pipeline-laajennos

--- protoni/laajennos/pipeline.py
+++ protoni/laajennos/pipeline.py
@@ -1,28 +1,28 @@
 # -*- coding: utf-8 -*-
 
 import pipeline
+from pipeline.signals import (
+  css_compressed,
+  js_compressed,
+)
+
+
 del pipeline
 
 INSTALLED_APPS.append('pipeline')
 
 STATICFILES_STORAGE = (
-  'pipeline.storage.PipelineManifestStorage'
+  'pipeline.storage.PipelineStorage'
 )
 
-#try:
-#  STATICFILES_FINDERS
-#except NameError:
-#  from django.conf.global_settings import (
-#    STATICFILES_FINDERS
-#  )
-#STATICFILES_FINDERS.append(
-#  'pipeline.finders.PipelineFinder'
-#)
-STATICFILES_FINDERS = (
-  'pipeline.finders.FileSystemFinder',
-  'pipeline.finders.AppDirectoriesFinder',
-  'pipeline.finders.CachedFileFinder',
-  'pipeline.finders.PipelineFinder',
+try:
+  STATICFILES_FINDERS
+except NameError:
+  from django.conf.global_settings import (
+    STATICFILES_FINDERS
+  )
+STATICFILES_FINDERS.append(
+  'pipeline.finders.PipelineFinder'
 )
 
 PIPELINE = {
@@ -34,3 +34,13 @@ PIPELINE = {
   'CSS_COMPRESSOR': 'pipeline.compressors.csshtmljsminify.CssHtmlJsMinifyCompressor',
   'JS_COMPRESSOR': 'pipeline.compressors.csshtmljsminify.CssHtmlJsMinifyCompressor',
 }
+
+# Poista JS- ja CSS-lähdekoodi paketoinnin jälkeen.
+def poista_lahdekoodi(sender, *, package, **kwargs):
+  for lahde in package.sources:
+    sender.compressor.storage.delete(lahde)
+css_compressed.connect(poista_lahdekoodi)
+js_compressed.connect(poista_lahdekoodi)
+
+del css_compressed
+del js_compressed
