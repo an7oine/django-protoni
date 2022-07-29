94e400674ae54e26696d7ba107d780485e632491
Antti Hautaniemi <antti.hautaniemi@pispalanit.fi>
2022-07-29 13:43:14 +0300

    Salasanan monimutkaisuusvaatimukset vain palvelinkäytössä

--- protoni/palvelin.py
+++ protoni/palvelin.py
@@ -39,3 +39,13 @@ except NameError:
   STATICFILES_STORAGE = (
     'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
   )
+
+# Salasanavaatimukset.
+AUTH_PASSWORD_VALIDATORS = [
+  {'NAME': (
+    'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'
+  )},
+  {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+  {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+  {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+]
