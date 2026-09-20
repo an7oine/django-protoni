97c5b3520da6e4c8555b5e1f4d834f98a89defd1
Antti Hautaniemi <antti.hautaniemi@tavusto.fi>
2026-09-20 10:55:56 +0300

    Korjattu dj_database_url-laajennos: tarvittaessa tyhjä DATABASES

--- protoni/laajennos/dj_database_url.py
+++ protoni/laajennos/dj_database_url.py
@@ -2,7 +2,15 @@
 
 import dj_database_url
 
+# Tietokanta-asetukset muodostuvat `protoni.asetukset`-moduulissa vain,
+# mikäli `DB_ENGINE` on määritetty. Luodaan tarvittaessa tyhjä pohja,
+# jotta pelkkä `DATABASE_URL` riittää määritykseksi (Heroku, Railway).
+try:
+  DATABASES
+except NameError:
+  DATABASES = {'default': {}}
+
 DATABASES['default'] = dj_database_url.config(
   conn_max_age=600,
-  ssl_require=True
+  ssl_require=CONFIG('DB_SSL_REQUIRE', cast=bool, default=True),
 ) or DATABASES['default']
